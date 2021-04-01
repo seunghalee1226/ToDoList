e0bd2f3b261ec26caf6ab6d093d68f033fbd4a47
Seungha Lee <shl1226@ezfarm.co.kr>
2021-04-01 18:25:42 +0900

    Input 생성

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import styled, { ThemeProvider } from 'styled-components/native';
 import { theme } from './theme';
+import { StatusBar } from 'react-native';
+import Input from './components/Input';
 
-const Container = styled.View`
+const Container = styled.SafeAreaView`
     flex: 1;
-    background-color: ${({ theme}) => theme.background};
+    background-color: ${({ theme }) => theme.background};
     align-items: center;
-    justify-content: center;
+    justify-content: flex-start;
+    `;
+
+const Title = styled.Text`
+    font-size: 40px;
+    font-weight: 600;
+    color: ${({ theme }) => theme.main};
+    align-self: flex-start;
+    margin: 0px 20px;
     `;
 
 export default function App() {
     return (
         <ThemeProvider theme={theme}>
-            <Container></Container>
+            <Container>
+                <StatusBar 
+                  barStyle="light-content"
+                  backgroundColor={theme.background}
+                />
+                <Title>TODO LIST</Title>
+                <Input />
+            </Container>
         </ThemeProvider>
     );
 }
